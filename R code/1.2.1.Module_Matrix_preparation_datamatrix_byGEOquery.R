224e9583c070f4325478a80c53a722e2838b93f4
Drinchai <drinchai@gmail.com>
2020-07-14 03:32:45 +0300

    Materials and supplementary files for Modular repertoire analysis

--- R code/1.2.1.Module_Matrix_preparation_datamatrix_byGEOquery.R
+++ R code/1.2.1.Module_Matrix_preparation_datamatrix_byGEOquery.R
@@ -1,11 +1,11 @@
-
 # Setup environment
 rm(list = ls())
 setwd("~/DC_Gen3_Module_analysis")
 
+library(preprocessCore)
 # Set parameters
 GSE_ID = "GSE13015"
-platform = "GPL6106" # "GPL6106" "GPL6947"   # Available platforms for each GSE dataset can be checked by running code below
+platform = "GPL6106" # "GPL6106" "GPL6947" # Available platforms for each GSE dataset can be checked by running code below
 
 # Load Module list ##
 load("./R data/DC_ModuleGen3_2019.Rdata")
@@ -15,26 +15,31 @@ load(paste0("./R data/", GSE_ID, "_", platform, "_data.matrix.Rdata"))
 data.matrix = as.data.frame(data.matrix)
 data.matrix$ID = rownames(data.matrix)
 
+##quantile normalization
+data.matrix.nor <- normalize.quantiles(as.matrix(data.matrix))
+colnames(data.matrix.nor) = colnames(data.matrix)
+rownames(data.matrix.nor) = rownames(data.matrix)
+
 ## prepare data at the gene level by aggregate value of each gene 
-data.matrix$Symbol = Probe.annotation.table$Symbol[match(data.matrix$ID, Probe.annotation.table$ID)]
-data.matrix = data.matrix[-which(data.matrix$Symbol == ""),]                                          # remove probes without annotated gene Symbol
-data.matrix$ID = NULL
-data.matrix = aggregate(data.matrix,FUN = mean,by=list(data.matrix$Symbol))                           # calculate average of each gene
-data.matrix$Symbol = NULL
-rownames(data.matrix) = data.matrix$Group.1
-data.matrix$Group.1 = NULL
+data.matrix.nor$Symbol = Probe.annotation.table$Symbol[match(data.matrix.nor$ID, Probe.annotation.table$ID)]
+data.data.matrix.nor = data.matrix.nor[-which(data.matrix.nor$Symbol == ""),]                                          # remove probes without annotated gene Symbol
+data.matrix.nor$ID = NULL
+data.matrix.nor = aggregate(data.matrix.nor,FUN = mean,by=list(data.matrix.nor$Symbol))                           # calculate average of each gene
+data.matrix.nor$Symbol = NULL
+rownames(data.matrix.nor) = data.matrix.nor$Group.1
+data.matrix.nor$Group.1 = NULL
 
 sample.info$Type = gsub(sample.info$Type,pattern = "type 2 diabetes",replacement = "T2D")             # Modify text
 sample.info$Type = gsub(sample.info$Type,pattern = "Other infections",replacement = "Other")          # Modify text
 
 # preparing data ##
-head(data.matrix)               # The data need to be chceked whether they are log2 transformed or raw data.
-rownames(data.matrix)           # check gene symbol
-data.matrix[data.matrix<10]=10  # fillter gene that has expression value < 10 = 10
+head(data.matrix.nor)                   # The data need to be chceked whether they are log2 transformed or raw data.
+rownames(data.matrix.nor)               # check gene symbol
+data.matrix.nor[data.matrix.nor<10]=10  # fillter gene that has expression value < 10 = 10
 
 ### Prepare expression matrix with module list
 df1=Module_listGen3
-df2=data.frame(data.matrix)
+df2=data.frame(data.matrix.nor)
 df2$Gene = rownames(df2)
 
 #Annotate gene module to expression matrix
